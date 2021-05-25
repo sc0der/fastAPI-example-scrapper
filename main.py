a5fcd59662ff580de98d211ddfd733f6b7c963cb
Sirius Sufiew <44986984+sc0der@users.noreply.github.com>
2021-05-25 18:07:23 +0500

    make it better

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
 
 # init Ninja Api
 app = FastAPI()
@@ -15,6 +16,15 @@ def scrapp_nbt(date):
     values = soup2.find_all('value')
     VALUTES = {keys[i].text:values[i].text for i in range(len(valutes)-1)}
     return VALUTES
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/nbt")
 def read_root():
