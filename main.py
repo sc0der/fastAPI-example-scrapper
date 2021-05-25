19adb728c20af881541619434c9db9ad9d8ae907
Sirius Sufiew <44986984+sc0der@users.noreply.github.com>
2021-05-25 17:25:57 +0500

    make it better

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+# init Ninja Api
+app = FastAPI()
+
+#---------------------------------#
+def scrapp_nbt(date):
+    nbt2 = requests.get(f"https://nbt.tj/ru/kurs/export_xml.php?date={date}&export=xmlout")
+    soup2 = BeautifulSoup(nbt2.content, 'lxml')
+    valutes = soup2.find_all('valute')
+    keys = soup2.find_all('charcode')
+    values = soup2.find_all('value')
+    VALUTES = {keys[i].text:values[i].text for i in range(len(valutes)-1)}
+    return VALUTES
+
+@app.get("/nbt")
+def read_root():
+    return scrapp_nbt(str(datetime.now().year) + "-" + str(datetime.now().month) + "-" + str(datetime.now().day))
